338ec0468da913f9ac99436914714e8d7ba77dc1
lucien <lucien.guimaraes@gmail.com>
2024-05-20 14:17:08 +0200

    Cleanup

--- app/src/main/java/com/hika/myjokes/MainActivity.kt
+++ app/src/main/java/com/hika/myjokes/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyJokesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold(modifier = Modifier.fillMaxSize()) {}
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyJokesTheme {
-        Greeting("Android")
     }
 }
