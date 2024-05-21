edc80d9b8727b679cdc8441d6caa7533f53a6154
lucien <lucien.guimaraes@gmail.com>
2024-05-21 20:07:04 +0200

    Added coverage for JokeViewModel

--- datasource/src/main/java/com/lucienguimaraes/datasource/entities/JokeEntity.kt
+++ datasource/src/main/java/com/lucienguimaraes/datasource/entities/JokeEntity.kt
@@ -15,3 +15,9 @@ fun jokeEntityNoFavorite() = JokeEntity(
     content = "There are only 10 kinds of people in this world: those who know binary and those who don't.",
     favorite = false
 )
+
+fun jokeEntityFavorite() = JokeEntity(
+    id = 5,
+    content = "There are only 10 kinds of people in this world: those who know binary and those who don't.",
+    favorite = true
+)
--- features/joke/src/test/java/com/lucienguimaraes/joke/JokeViewModelTest.kt
+++ features/joke/src/test/java/com/lucienguimaraes/joke/JokeViewModelTest.kt
@@ -1,7 +1,8 @@
 package com.lucienguimaraes.joke
 
-import com.lucienguimaraes.datasource.entities.JokeEntity
 import com.lucienguimaraes.datasource.JokeRepository
+import com.lucienguimaraes.datasource.entities.JokeEntity
+import com.lucienguimaraes.datasource.entities.jokeEntityFavorite
 import com.lucienguimaraes.datasource.entities.jokeEntityNoFavorite
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -18,15 +19,11 @@ internal class JokeViewModelTest {
         Dispatchers.setMain(Dispatchers.Unconfined)
     }
 
+    //region fetch joke
     @Test
     fun `Given a click on fetch joke it should get a new joke`() {
-        class TestJokeRepository : JokeRepository {
-            override suspend fun fetchJoke(): Result<JokeEntity> = Result.success(
-                jokeEntityNoFavorite()
-            )
-        }
 
-        val viewModel = JokeViewModel(TestJokeRepository())
+        val viewModel = JokeViewModel(SuccessJokeRepository())
         assertEquals(JokeUIState(), viewModel.uiState.value)
         viewModel.fetchNewJoke()
         assertEquals(JokeUIState(joke = jokeEntityNoFavorite()), viewModel.uiState.value)
@@ -34,11 +31,7 @@ internal class JokeViewModelTest {
 
     @Test
     fun `Given a click on fetch joke that fail it should set an error on ui state`() {
-        class TestJokeRepository : JokeRepository {
-            override suspend fun fetchJoke(): Result<JokeEntity> = Result.failure(Exception())
-        }
-
-        val viewModel = JokeViewModel(TestJokeRepository())
+        val viewModel = JokeViewModel(ErrorJokeRepository())
         assertEquals(JokeUIState(), viewModel.uiState.value)
         viewModel.fetchNewJoke()
         assertEquals(JokeUIState(error = true), viewModel.uiState.value)
@@ -46,15 +39,84 @@ internal class JokeViewModelTest {
 
     @Test
     fun `Given a dismiss on error it should remove the error from the state`() {
-        class TestJokeRepository : JokeRepository {
-            override suspend fun fetchJoke(): Result<JokeEntity> = Result.failure(Exception())
-        }
-
-        val viewModel = JokeViewModel(TestJokeRepository())
+        val viewModel = JokeViewModel(ErrorJokeRepository())
         assertEquals(JokeUIState(), viewModel.uiState.value)
         viewModel.fetchNewJoke()
         assertEquals(JokeUIState(error = true), viewModel.uiState.value)
         viewModel.dismissError()
         assertEquals(JokeUIState(), viewModel.uiState.value)
     }
+    //endregion
+
+    //region favorite click
+    @Test
+    fun `Given a click on favorite it should save the joke`() {
+        val viewModel = JokeViewModel(SuccessJokeRepository())
+        assertEquals(JokeUIState(), viewModel.uiState.value)
+        viewModel.fetchNewJoke()
+        assertEquals(JokeUIState(joke = jokeEntityNoFavorite()), viewModel.uiState.value)
+        viewModel.onFavoriteClick()
+        assertEquals(JokeUIState(joke = jokeEntityFavorite()), viewModel.uiState.value)
+    }
+
+    @Test
+    fun `Given a click on favorite with already as favorite it should delete the joke`() {
+        val viewModel = JokeViewModel(SuccessJokeRepository())
+        assertEquals(JokeUIState(), viewModel.uiState.value)
+        viewModel.fetchNewJoke()
+        assertEquals(JokeUIState(joke = jokeEntityNoFavorite()), viewModel.uiState.value)
+        viewModel.onFavoriteClick()
+        assertEquals(JokeUIState(joke = jokeEntityFavorite()), viewModel.uiState.value)
+        viewModel.onFavoriteClick()
+        assertEquals(JokeUIState(joke = jokeEntityNoFavorite()), viewModel.uiState.value)
+    }
+
+    @Test
+    fun `Given a click on favorite with a null joke it should set an error`() {
+        val viewModel = JokeViewModel(SuccessJokeRepository())
+
+        viewModel.onFavoriteClick()
+        assertEquals(JokeUIState(error = true), viewModel.uiState.value)
+    }
+
+    @Test
+    fun `Given a click on favorite with an error to save favorite it should set an error`() {
+        class CustomJokeRepository : JokeRepository {
+            override suspend fun fetchJoke(): Result<JokeEntity> =
+                Result.success(jokeEntityNoFavorite())
+
+            override suspend fun saveJoke(joke: JokeEntity): Result<JokeEntity> =
+                Result.failure(Exception())
+
+            override suspend fun deleteJoke(joke: JokeEntity): Result<JokeEntity> =
+                Result.failure(Exception())
+        }
+        val viewModel = JokeViewModel(CustomJokeRepository())
+        assertEquals(JokeUIState(), viewModel.uiState.value)
+        viewModel.fetchNewJoke()
+        assertEquals(JokeUIState(joke = jokeEntityNoFavorite()), viewModel.uiState.value)
+        viewModel.onFavoriteClick()
+        assertEquals(JokeUIState(joke = jokeEntityNoFavorite(), error = true), viewModel.uiState.value)
+    }
+
+    @Test
+    fun `Given a click on favorite with an error to delete favorite it should set an error`() {
+        class CustomJokeRepository : JokeRepository {
+            override suspend fun fetchJoke(): Result<JokeEntity> =
+                Result.success(jokeEntityFavorite())
+
+            override suspend fun saveJoke(joke: JokeEntity): Result<JokeEntity> =
+                Result.failure(Exception())
+
+            override suspend fun deleteJoke(joke: JokeEntity): Result<JokeEntity> =
+                Result.failure(Exception())
+        }
+        val viewModel = JokeViewModel(CustomJokeRepository())
+        assertEquals(JokeUIState(), viewModel.uiState.value)
+        viewModel.fetchNewJoke()
+        assertEquals(JokeUIState(joke = jokeEntityFavorite()), viewModel.uiState.value)
+        viewModel.onFavoriteClick()
+        assertEquals(JokeUIState(joke = jokeEntityFavorite(), error = true), viewModel.uiState.value)
+    }
+    //endregion
 }
